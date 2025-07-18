19ec400581090d170ef766eac069fa880fe3c720
Jeff Conrad <jeff.conrad@gmx.de>
2025-07-18 19:04:20 +0200

    Configure GitHub Pages deployment

--- src/pages/[lang]/rss.xml.ts
+++ src/pages/[lang]/rss.xml.ts
@@ -1,5 +1,12 @@
 import type { APIContext } from 'astro'
 import { generateRSS } from '@/utils/feed'
+import { langMap } from '@/i18n/config'
+
+export async function getStaticPaths() {
+  return Object.keys(langMap).map((lang) => ({
+    params: { lang },
+  }))
+}
 
 export async function GET(context: APIContext) {
   return generateRSS(context)
